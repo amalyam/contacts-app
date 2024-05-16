69df871e87f03c32c766c291080b2a96f55c3fbd
Amalya Megerman <115735046+amalyam@users.noreply.github.com>
2024-05-16 18:43:45 +0000

    set up form to add new contact

---
+++ frontend/src/CreateContactForm.tsx
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+import { Contact } from "./types/Contact";
+
+export default function CreateContactForm({}) {
+  const [contactData, setContactData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+  });
+
+  const onSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    const data = {
+      contactData,
+    };
+
+    const url = process.env.REACT_APP_API_URL + "/create_contact";
+
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    };
+    const response = await fetch(url, options);
+    if (response.status !== 201 && response.status !== 200) {
+      const data = await response.json();
+      alert(data.message);
+    } else {
+      // successful
+    }
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setContactData({
+      ...contactData,
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <div>
+        <label htmlFor="firstName">First Name:</label>
+        <input
+          type="text"
+          id="firstName"
+          value={contactData.firstName}
+          onChange={handleChange}
+        />
+      </div>
+      <label htmlFor="lastName">Last Name:</label>
+      <input
+        type="text"
+        id="lastName"
+        value={contactData.lastName}
+        onChange={handleChange}
+      />
+      <label htmlFor="email">Email:</label>
+      <input
+        type="email"
+        id="email"
+        value={contactData.email}
+        onChange={handleChange}
+      />
+      <button type="submit">Create Contact</button>
+    </form>
+  );
+}
