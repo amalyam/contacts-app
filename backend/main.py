6e08d773419da12882206a91e01b8fcc75d60675
Amalya Megerman <115735046+amalyam@users.noreply.github.com>
2024-05-16 14:58:37 +0000

    create GET route

---
+++ backend/main.py
@@ -0,0 +1,10 @@
+from flask import request, jsonify
+from config import app, db
+from models import Contact
+
+
+@app.route("/contacts", methods=["GET"])
+def get_contacts():
+    contacts = Contact.query.all()
+    json_contacts = list(map(lambda x: x.to_json(), contacts))
+    return jsonify({"contacts": json_contacts})
