5672eb7770fc739c78aa4a256f7245468267c4c8
Amalya Megerman <115735046+amalyam@users.noreply.github.com>
2024-05-16 22:49:53 +0000

    fix config files to address errors

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { createRequire } from "module";
+
+const requireFunc = createRequire(import.meta.url);
 
 export default defineConfig({
   plugins: [react()],
   css: {
     postcss: {
-      plugins: [require("tailwindcss"), require("autoprefixer")],
+      plugins: [requireFunc("tailwindcss"), requireFunc("autoprefixer")],
     },
   },
 });
