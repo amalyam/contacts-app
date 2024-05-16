9a56502bb380a1dca5c7b8f96260f1de82af8195
Amalya Megerman <115735046+amalyam@users.noreply.github.com>
2024-05-16 17:04:18 +0000

    create basic contact list component

---
+++ frontend/src/ContactList.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Contact, ContactListProps } from "./types/Contact";
+
+export default function ContactList({ contacts }: ContactListProps) {
+  return (
+    <div>
+      <h2>Contacts</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Email</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {contacts.map((contact) => {
+            return (
+              <tr key={contact.id}>
+                <td>{contact.firstName}</td>
+                <td>{contact.lastName}</td>
+                <td>{contact.email}</td>
+                <td>
+                  <button>Update</button>
+                  <button>Delete</button>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+}
