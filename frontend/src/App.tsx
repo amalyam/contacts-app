8939632d462e81fefc2a6f16e976d1bbdde92623
Amalya Megerman <115735046+amalyam@users.noreply.github.com>
2024-05-18 01:03:03 +0000

    use url instead of env variable

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,7 +10,7 @@ function App() {
   }, []);
 
   const fetchContacts = async () => {
-    const response = await fetch(process.env.REACT_APP_API_URL + "/contacts");
+    const response = await fetch("http://127.0.0.1:5000/contacts");
     const data = await response.json();
     setContacts(data.contacts);
     console.log(data.contacts);
